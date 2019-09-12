f79931f695a86dd199c4e9f583723f16ab07c66b
fredvunkiktkhk <fred.vunk@khk.ee>
2019-09-12 15:46:30 +0300

    Updated all end points

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const restify = require("restify");
 const mongoose = require("mongoose");
 const config = require("./config");
 const rjwt = require("restify-jwt-community");
+const m2s = require('mongoose-to-swagger');
+const food = mongoose.model('food', {
+  name: string
+});
+const swaggerSchema = m2s(food);
+console.log(swaggerSchema);
 
 const server = restify.createServer();
 
@@ -13,7 +19,9 @@ server.use(restify.plugins.bodyParser());
 
 server.listen(config.PORT, () => {
   mongoose.set("useFindAndModify", false);
-  mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
+  mongoose.connect(config.MONGODB_URI, {
+    useNewUrlParser: true
+  });
 });
 
 const db = mongoose.connection;
@@ -23,6 +31,6 @@ db.on("error", err => console.log(err));
 db.once("open", () => {
   require("./routes/customers")(server);
   require("./routes/users")(server);
-  require("./routes/food")(server);
+  require("./routes/foods")(server);
   console.log(`Server started on port ${config.PORT}`);
 });
---
+++ routes/foods.js
@@ -0,0 +1,103 @@
+const errors = require("restify-errors");
+const rjwt = require("restify-jwt-community");
+const Food = require("../models/Food");
+const config = require("../config");
+
+module.exports = server => {
+  // Get food
+
+  server.get("/foods", async (req, res, next) => {
+    try {
+      const food = await Food.find({});
+      res.send(food);
+      next();
+    } catch (err) {
+      return next(new errors.InvalidContentError(err));
+    }
+  });
+
+  // Post food
+  server.post(
+    "/foods",
+    // rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is("application/json")) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
+      }
+
+      const {
+        name,
+        price
+      } = req.body;
+
+      const food = new Food({
+        name,
+        price
+      });
+
+      try {
+        const newFood = await food.save();
+        res.send(201);
+        next();
+      } catch (err) {
+        return next(new errors.InternalError(err.message));
+      }
+    }
+  );
+
+  // Update Food
+  server.put(
+    "/foods/:id",
+    // rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is("application/json")) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
+      }
+
+      try {
+        const food = await Food.findOneAndUpdate({
+            _id: req.params.id
+          },
+          req.body
+        );
+        res.send(200);
+        next();
+      } catch (err) {
+        return next(
+          new errors.ResourceNotFoundError(
+            `There is no food with the id of ${req.params.id}`
+          )
+        );
+      }
+    }
+  );
+
+  // Delete Food
+  server.del(
+    "/foods/:id",
+    // rjwt({
+    //  secret: config.JWT_SECRET
+    //  }),
+    async (req, res, next) => {
+      try {
+        const food = await Food.findOneAndRemove({
+          _id: req.params.id
+        });
+        res.send(204);
+        next();
+      } catch (err) {
+        return next(
+          new errors.ResourceNotFoundError(
+            `There is no food with the id of ${req.params.id}`
+          )
+        );
+      }
+    }
+  );
+};
